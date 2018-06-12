2e29c89ea21c15a74de954a3f421125cd9651a68
frances-ma <36893566+frances-ma@users.noreply.github.com>
2018-06-11 20:58:58 -0400

    Update table.js

--- table.js
+++ table.js
@@ -1,16 +1,3 @@
-/*
-JQUERY LAB PART 2
-// Task: Add on to the seating application by taking the data the user entered into the form and
-associating it to the table. Include transitions for the form being hidden and displayed.
-What does the application do?
-1. When a user submits information through the form and mouses over the table they
-reserved, display that it was reserved using the user’s name and the size of the party.
-2. Displays animated transitions for when the form is hidden or displayed.
-Build Specifications:
-1. Must include transitions to hide and show the form.
-2. Assigns data to the element(s) that were reserved.
-3. Is responsive.
-*/ 
 
 
 "use strict"; 
